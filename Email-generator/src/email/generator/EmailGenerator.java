49010bdd7e95972922ff3a03cc9219f8a89230c4
2023154 <2023154@student.cct.ie>
2025-04-03 12:37:50 +0100

    #1 - import bufferReader

---
+++ Email-generator/src/email/generator/EmailGenerator.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package email.generator;
+
+import java.io.BufferedWriter;
+
+/**
+ *
+ * @author Marcos
+ */
+
+    
+public class EmailGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static BufferedWriter writer;//add buffer to write the email to the file
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+}
